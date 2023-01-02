f5881faf8c52f6b2b46ec82b937ae63890bc8722
Anton Lobanov <alobanov11@gmail.com>
2023-01-02 15:00:55 +0300

    Fix

--- Habbitio/Extensions/Habit+Rate.swift
+++ Habbitio/Extensions/Habit+Rate.swift
@@ -6,7 +6,7 @@ import Foundation
 
 extension Habit {
     func rate(for number: Int) -> Double {
-        let records = (self.records?.suffix(number).compactMap { $0 as? Record } ?? []).filter { $0.isEnabled }
+        let records = (self.records?.suffix(number).compactMap { $0 as? Record } ?? []).filter { $0.isEnabled || $0.done }
         let count = records.filter { $0.done }.count
         let total = records.count
         return total == 0 ? 0 : Double(count) / Double(total)
--- Habbitio/Extensions/Report+Rate.swift
+++ Habbitio/Extensions/Report+Rate.swift
@@ -6,7 +6,7 @@ import Foundation
 
 extension Report {
     var rate: Double {
-        let records = (self.records?.compactMap { $0 as? Record } ?? []).filter { $0.isEnabled }
+        let records = (self.records?.compactMap { $0 as? Record } ?? []).filter { $0.isEnabled || $0.done }
         let count = records.filter { $0.done }.count
         let total = records.count
         return total == 0 ? 0 : Double(count) / Double(total)
